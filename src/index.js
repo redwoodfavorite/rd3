1e073bf44ca0dc04052a4d9aac56acb511059c00
Joseph Sample <redwood@jojo.local>
2016-10-27 14:35:22 -0700

    Adding common components to package exports

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ exports.Treemap = require('./treemap').Treemap;
 exports.ScatterChart = require('./scatterchart').ScatterChart;
 exports.CandlestickChart = require('./candlestick').CandlestickChart;
 
+exports.XAxis = require('./common');
+exports.YAxis = require('./common');
+exports.Chart = require('./common');
+exports.LegendChart = require('./common');
+exports.Legend = require('./common');
+exports.Tooltip = require('./common');
+exports.Voronoi = require('./common');
